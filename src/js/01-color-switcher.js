bfa4fb403e7d87d7633b97fdf6d78bc0908e7452
Serhii1727 <kravchenko087@ukr.net>
2023-10-25 22:55:27 -0400

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
-console.log('Hello')
+const refs = {
+    body: document.querySelector('body'),
+    btnStart: document.querySelector('button[data-action="start"]'),
+    btnStop: document.querySelector('button[data-action="stop"]')
+}
+
+let currentInrervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const startChangeColorBody = () => {
+    
+    currentInrervalId = setInterval(() => {
+        const color = getRandomHexColor();
+        refs.body.style.backgroundColor = color;
+    }, 1000)
+
+    refs.btnStart.disabled = true;
+}
+
+const stopChangeColorBody = () => {
+    clearInterval(currentInrervalId)
+    refs.btnStart.disabled = false;
+    refs.btnStop.disabled = true;
+}
+
+refs.btnStart.addEventListener('click', startChangeColorBody)
+refs.btnStop.addEventListener('click', stopChangeColorBody)
